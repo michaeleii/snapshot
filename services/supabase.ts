9a2f643b00cc559ae6bcd72ba9f864027726f848
Michael Lei <michaelleie0ca@gmail.com>
2023-07-27 16:37:40 -0700

    Add supabase url

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,11 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "../types/schema";
 
-export const supabaseUrl = process.env.SUPABASE_URL;
+export const supabaseUrl = "https://lzrslhygdxwyshinircf.supabase.co";
 const supabaseKey = process.env.SUPABASE_KEY;
 
-if (!supabaseUrl || !supabaseKey)
-  throw new Error("Missing SUPABASE_URL or SUPABASE_KEY");
+if (!supabaseKey) throw new Error("Missing SUPABASE_KEY");
 
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 

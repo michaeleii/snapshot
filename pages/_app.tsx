771f15474e0a698c79488185efe7d4178b212460
Michael Lei <michaelleie0ca@gmail.com>
2023-07-27 15:37:02 -0700

    Add react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,24 @@ import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const inter = Inter({
   subsets: ["latin"],
 });
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <main className={inter.className}>
-        <Component {...pageProps} />
-      </main>
+      <QueryClientProvider client={queryClient}>
+        <main className={inter.className}>
+          <Component {...pageProps} />
+        </main>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }

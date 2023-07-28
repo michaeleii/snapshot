681db5e94e46f15d9a519ed0a7c656763d5093a4
Michael Lei <michaelleie0ca@gmail.com>
2023-07-27 18:32:53 -0700

    Add image tag

--- components/ui/AuthLayout.tsx
+++ components/ui/AuthLayout.tsx
@@ -1,14 +1,18 @@
 import AlreadyLoggedIn from "../AlreadyLoggedIn";
+import Image from "next/image";
 function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <AlreadyLoggedIn>
       <div className="grid grid-cols-1 xl:grid-cols-2">
         {children}
-        <img
-          src="/test-post.jpg"
-          alt=""
-          className="hidden h-screen w-full object-cover xl:block"
-        />
+        <div className="relative">
+          <Image
+            src="/test-post.jpg"
+            alt=""
+            fill
+            className="hidden object-cover xl:block"
+          />
+        </div>
       </div>
     </AlreadyLoggedIn>
   );

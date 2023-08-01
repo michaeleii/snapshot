63756992c405794217c559ad597e8ad2a923c77c
Michael Lei <michaelleie0ca@gmail.com>
2023-07-31 21:04:35 -0700

    Drawer for settings

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -8,7 +8,7 @@ import { useLogout } from "@/hooks/useLogout";
 import { Drawer } from "vaul";
 
 import { Settings } from "lucide-react";
-import LoginForm from "@/pages/login/LoginForm";
+import SettingsForm from "@/pages/settings";
 
 function MainNav() {
   const { currentUser, isAuthenticated } = useUser();
@@ -16,80 +16,87 @@ function MainNav() {
   const { mutate: logout, isLoading: isLoggingOut } = useLogout();
 
   return (
-    <nav className="p-5 xl:p-10 xl:border-r xl:border-b-0 border-b xl:min-h-screen flex flex-col relative">
-      <div className="absolute right-2 top-2 block xl:hidden">
-        <ModeToggle />
-      </div>
-      {isAuthenticated && (
-        <div className="absolute left-2 top-2 block xl:hidden">
-          <Button asChild variant="ghost" className="w-full xl:hidden">
+    <Drawer.Root>
+      <nav className="p-5 xl:p-10 xl:border-r xl:border-b-0 border-b xl:min-h-screen flex flex-col relative">
+        <div className="absolute right-2 top-2 block xl:hidden">
+          <ModeToggle />
+        </div>
+        {isAuthenticated && (
+          <div className="absolute left-2 top-2 block xl:hidden">
+            {/* <Button asChild variant="ghost" className="w-full xl:hidden">
             <Link href="/settings">
               <Settings />
             </Link>
-          </Button>
-        </div>
-      )}
-      <div className="p-5">
-        <Logo />
-      </div>
-      {!isAuthenticated && (
-        <div className="flex xl:flex-col gap-2 mt-auto mb-5">
-          {/* <Drawer.Root>
+          </Button> */}
             <Drawer.Trigger asChild>
-              <Button>Login</Button>
+              <Button variant="ghost" className="xl:hidden w-full">
+                <Settings />
+              </Button>
+            </Drawer.Trigger>
+          </div>
+        )}
+        <div className="p-5">
+          <Logo />
+        </div>
+        {!isAuthenticated && (
+          <div className="flex xl:flex-col gap-2 mt-auto mb-5">
+            <Button asChild className="w-full">
+              <Link href="/login">Login</Link>
+            </Button>
+            <Button asChild variant="outline" className="w-full">
+              <Link href="/register">Register</Link>
+            </Button>
+          </div>
+        )}
+        {isAuthenticated && currentUser && (
+          <div className="hidden xl:flex xl:flex-col gap-2 mt-8 mb-5 mx-auto items-center">
+            <Image
+              src="/default-user.jpg"
+              alt=""
+              width={100}
+              height={100}
+              className="rounded-full object-cover object-center"
+            />
+            <span>{currentUser.username}</span>
+          </div>
+        )}
+        {isAuthenticated && (
+          <div className="flex xl:flex-col gap-2 mt-auto mb-5">
+            <Button asChild className="w-full">
+              <Link href="/upload">Create Post</Link>
+            </Button>
+
+            <Drawer.Trigger asChild>
+              <Button className="hidden xl:block">Settings</Button>
             </Drawer.Trigger>
 
             <Drawer.Portal>
               <Drawer.Overlay className="fixed inset-0 bg-black/40" />
               <Drawer.Content className="bg-background flex flex-col rounded-t-[10px] h-[50%] mt-24 fixed bottom-0 left-0 right-0">
                 <div className="p-4 bg-background rounded-t-[10px] flex-1">
-                  <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-primary mb-8" />
+                  <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-primary mb-8 bg-zinc-400" />
                   <div className="max-w-md mx-auto">
-                    <LoginForm />
+                    <SettingsForm />
                   </div>
                 </div>
               </Drawer.Content>
             </Drawer.Portal>
-          </Drawer.Root> */}
-          <Button asChild className="w-full">
-            <Link href="/login">Login</Link>
-          </Button>
-          <Button asChild variant="outline" className="w-full">
-            <Link href="/register">Register</Link>
-          </Button>
-        </div>
-      )}
-      {isAuthenticated && currentUser && (
-        <div className="hidden xl:flex xl:flex-col gap-2 mt-8 mb-5 mx-auto items-center">
-          <Image
-            src="/default-user.jpg"
-            alt=""
-            width={100}
-            height={100}
-            className="rounded-full object-cover object-center"
-          />
-          <span>{currentUser.username}</span>
-        </div>
-      )}
-      {isAuthenticated && (
-        <div className="flex xl:flex-col gap-2 mt-auto mb-5">
-          <Button asChild className="w-full">
-            <Link href="/upload">Create Post</Link>
-          </Button>
-          <Button variant="outline" asChild className="w-full hidden xl:flex">
+
+            {/* <Button variant="outline" asChild className="w-full hidden xl:flex">
             <Link href="/settings">Settings</Link>
-          </Button>
-          <Button
-            variant="outline"
-            className="w-full"
-            disabled={isLoggingOut}
-            onClick={() => logout()}
-          >
-            {isLoggingOut ? "Logging out" : "Logout"}
-          </Button>
-        </div>
-      )}
-    </nav>
+          </Button> */}
+            <Button
+              variant="outline"
+              className="w-full"
+              disabled={isLoggingOut}
+              onClick={() => logout()}
+            >
+              {isLoggingOut ? "Logging out" : "Logout"}
+            </Button>
+          </div>
+        )}
+      </nav>
+    </Drawer.Root>
   );
 }
 export default MainNav;
--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -14,7 +14,53 @@ import { useUser } from "@/contexts/UserContext";
 import { useUpdateUser } from "@/hooks/useUpdateUser";
 import { useEffect, useState } from "react";
 
-function Settings() {
+// function Settings() {
+//   const { currentUser } = useUser();
+//   const [username, setUsername] = useState("");
+//   const { mutate: updateUser, isLoading: isUpdating } = useUpdateUser();
+
+//   useEffect(() => {
+//     if (currentUser) setUsername(currentUser.username);
+//   }, [currentUser]);
+
+//   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     const username = e.currentTarget.username.value;
+//     updateUser({ username });
+//   };
+//   return (
+//     <ProtectedRoute>
+//       <MainLayout>
+//         <div className="xl:min-h-screen w-full flex justify-center items-center xl:border mt-20 xl:mt-0">
+//           <Card className="max-w-lg xl:border border-0">
+//             <CardHeader>
+//               <CardTitle>Change username</CardTitle>
+//             </CardHeader>
+//             <CardContent>
+//               <form className="space-y-5" onSubmit={handleSubmit}>
+//                 <div className="grid w-full items-center gap-4">
+//                   <Label>Username</Label>
+//                   <Input
+//                     id="username"
+//                     type="text"
+//                     required
+//                     value={username}
+//                     onChange={(e) => setUsername(e.target.value)}
+//                   />
+//                 </div>
+//                 <Button className="w-full" disabled={isUpdating}>
+//                   {isUpdating ? "Updating..." : "Change"}
+//                 </Button>
+//               </form>
+//             </CardContent>
+//           </Card>
+//         </div>
+//       </MainLayout>
+//     </ProtectedRoute>
+//   );
+// }
+
+function SettingsForm() {
   const { currentUser } = useUser();
   const [username, setUsername] = useState("");
   const { mutate: updateUser, isLoading: isUpdating } = useUpdateUser();
@@ -29,34 +75,28 @@ function Settings() {
     updateUser({ username });
   };
   return (
-    <ProtectedRoute>
-      <MainLayout>
-        <div className="xl:min-h-screen w-full flex justify-center items-center xl:border mt-20 xl:mt-0">
-          <Card className="max-w-lg xl:border border-0">
-            <CardHeader>
-              <CardTitle>Change username</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <form className="space-y-5" onSubmit={handleSubmit}>
-                <div className="grid w-full items-center gap-4">
-                  <Label>Username</Label>
-                  <Input
-                    id="username"
-                    type="text"
-                    required
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-                </div>
-                <Button className="w-full" disabled={isUpdating}>
-                  {isUpdating ? "Updating..." : "Change"}
-                </Button>
-              </form>
-            </CardContent>
-          </Card>
-        </div>
-      </MainLayout>
-    </ProtectedRoute>
+    <Card className="max-w-lg border-0">
+      <CardHeader>
+        <CardTitle>Change username</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form className="space-y-5" onSubmit={handleSubmit}>
+          <div className="grid w-full items-center gap-4">
+            <Label>Username</Label>
+            <Input
+              id="username"
+              type="text"
+              required
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <Button className="w-full" disabled={isUpdating}>
+            {isUpdating ? "Updating..." : "Change"}
+          </Button>
+        </form>
+      </CardContent>
+    </Card>
   );
 }
-export default Settings;
+export default SettingsForm;

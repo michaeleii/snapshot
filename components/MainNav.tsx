0c4d1425c54b6ed908f4f3a0cde3f7139a2f7f46
Michael Lei <michaelleie0ca@gmail.com>
2023-07-31 21:11:04 -0700

    Change height

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -72,7 +72,7 @@ function MainNav() {
 
             <Drawer.Portal>
               <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-              <Drawer.Content className="bg-background flex flex-col rounded-t-[10px] h-[60%] mt-24 fixed bottom-0 left-0 right-0">
+              <Drawer.Content className="bg-background flex flex-col rounded-t-[10px] min-h-[85vh] mt-24 fixed bottom-0 left-0 right-0">
                 <div className="p-4 bg-background rounded-t-[10px] flex-1">
                   <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-primary mb-8 bg-zinc-400" />
                   <div className="max-w-md mx-auto">

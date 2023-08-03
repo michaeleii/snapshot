0846b3b7630937dc1b83aead947c158e5c4a0dfb
Michael Lei <michaelleie0ca@gmail.com>
2023-08-02 17:58:37 -0700

    Edit

--- next.config.js
+++ next.config.js
@@ -7,28 +7,6 @@ const nextConfig = {
       "oaidalleapiprodscus.blob.core.windows.net",
     ],
   },
-  headers: async () => [
-    {
-      // matching all API routes
-      source: "/create/generate",
-      headers: [
-        { key: "Access-Control-Allow-Credentials", value: "true" },
-        {
-          key: "Access-Control-Allow-Origin",
-          value: process.env.NEXT_PUBLIC_API_URL,
-        },
-        {
-          key: "Access-Control-Allow-Methods",
-          value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-        },
-        {
-          key: "Access-Control-Allow-Headers",
-          value:
-            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-        },
-      ],
-    },
-  ],
 };
 
 module.exports = nextConfig;
